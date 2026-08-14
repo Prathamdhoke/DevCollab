73d4d86552eb2e8c6395e95f38def93cbfa72209
Pratham Dhoke <prathamcode01@gmail.com>
2026-08-14 16:08:33 +0530

    Fix case sensitive imports for Vercel

--- DevCollab/Frontend/src/App.jsx
+++ DevCollab/Frontend/src/App.jsx
@@ -10,7 +10,7 @@ import Home from "./pages/Home";
 
 import Login from "./pages/Login";
 
-import Signup from "./pages/Signup";
+import Signup from "./pages/SignUp";
 
 import NotFound from "./pages/NotFound";
 
