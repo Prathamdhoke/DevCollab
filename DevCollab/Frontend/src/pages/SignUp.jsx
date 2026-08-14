78665e0735743bdd7c113c0a42269f5034049388
Pratham Dhoke <prathamcode01@gmail.com>
2026-08-14 18:19:09 +0530

    Fix SignUp css casing

--- DevCollab/Frontend/src/pages/SignUp.jsx
+++ DevCollab/Frontend/src/pages/SignUp.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 import { useAuth } from "../context/AuthContext.jsx";
 
-import "./Signup.css";
+import "./SignUp.css";
 
 const Signup = () => {
   const navigate = useNavigate();
